ec35ccfa86239836ee4147975b40211c5e06455c
hppyb33 <ajuliasolis@icloud.com>
2023-05-06 15:17:37 -0700

    Implemented API

--- functions.js
+++ functions.js
@@ -1,3 +1,22 @@
 function search_results() {
     alert("Under Construction!!");
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON
+    fetch('https://data.usajobs.gov/api/codelist/agencysubelements').then(resp => resp.json()).then(jobListingData => {
+        console.log(jobListingData);
+
+
+        // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
+        jobListingData['CodeList'][0]['ValidValue'].forEach(job => {
+
+            // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
+            document.getElementById('display').innerHTML  += `
+            <div>
+                <p>${job['Value']}</p>
+                <p>${job['Acronym']}</p>
+            </div>
+            `;
+
+        });
+        // document.get
+    })
 }
