11084a99336671e1af62d136f921a3653d3e690e
hppyb33 <ajuliasolis@icloud.com>
2023-05-07 11:23:18 -0700

    category tab

--- functions.js
+++ functions.js
@@ -4,7 +4,7 @@ function search_results() {
     div.remove();
     document.getElementById('jobs-in').innerHTML = "Jobs located in " + location;
 
-    var map = L.map('map').setView([51.505, -0.09], 13);
+    var map = L.map('map').setView([33.6424, -117.8417], 13);
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
