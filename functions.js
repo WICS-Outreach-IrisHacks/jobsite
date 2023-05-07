7474e9246f47c368a7c8ebd771fbc3d63f8b1732
hppyb33 <ajuliasolis@icloud.com>
2023-05-07 11:03:50 -0700

    added map

--- functions.js
+++ functions.js
@@ -1,5 +1,17 @@
 function search_results() {
-    alert("Under Construction!!");
+    var div = document.getElementById("search");
+    var location = document.getElementById("search_input").value;
+    div.remove();
+    document.getElementById('jobs-in').innerHTML = "Jobs located in " + location;
+
+    var map = L.map('map').setView([51.505, -0.09], 13);
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+
+
     // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON
     fetch('https://data.usajobs.gov/api/codelist/agencysubelements').then(resp => resp.json()).then(jobListingData => {
         console.log(jobListingData);
